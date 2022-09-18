9823f114db4c30be080566f4fec95787681cfc74
Vladislav izmailov <arielmap@icloud.com>
2022-09-18 16:53:45 +0300

    add task

---
+++ task/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array;
+Console.WriteLine("Введите массив строк через пробел");
+string line = Console.ReadLine();
+array = line.Split(' ');
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
